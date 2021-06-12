28546811b9ed94a2f3d934d86ab2b7ac599b37c7
arthur orduh <arthurorduh@gmail.com>
2021-06-12 08:47:10 +0100

    validation setup

--- src/api/routes/v1/auth.route.js
+++ src/api/routes/v1/auth.route.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const validate = require('express-validation');
+const {
+    login,
+    register,
+  } = require('../../validation/auth.validation');
 const authController = require('../../controller/auth.controller');
 
 const router = express.Router();
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', validate(register), authController.register);
+router.post('/login', validate(login), authController.login);
 
 module.exports = router;
